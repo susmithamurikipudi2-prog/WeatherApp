a1e3accb0ec7c4a25b3aa6561bf841ac42a339e3
susmithamurikipudi2-prog <susmithamurikipudi2@gmail.com>
2026-01-18 11:48:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function getWeather() {
+  let city = document.getElementById("city").value;
+  let apiKey = 
+    "8aa5913cf8d464edc73b3d80e4e9e350";
+  if (city === "") {
+    document.getElementById("result").innerText = "Please enter city name";
+    return;
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.cod === 200) {
+        document.getElementById("result").innerHTML = `
+          <h3>${data.name}</h3>
+          <p>🌡 Temperature: ${data.main.temp} °C</p>
+          <p>☁ Weather: ${data.weather[0].main}</p>
+          <p>💨 Wind Speed: ${data.wind.speed} m/s</p>
+        `;
+      } else {
+        document.getElementById("result").innerText = "City not found";
+      }
+    })
+    .catch(error => {
+      document.getElementById("result").innerText = "Error fetching data";
+    });
+}
